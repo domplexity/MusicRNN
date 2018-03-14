50d19698472300c824886d4cd0bf764a332ccec3
dominique <dominique.vandamme@neuraum.com>
2018-03-14 19:48:57 +0100

    new hyperparams

--- hypersearch.py
+++ hypersearch.py
@@ -4,9 +4,9 @@ import os
 from train import main
 
 
-seq_lens = [100, 1000]
-layers = [1, 2]
-hidden_sizes = [100, 200]
+seq_lens = [2000, 3000]
+layers = [2]
+hidden_sizes = [200, 300]
 
 #for all combinations:
 
@@ -14,7 +14,7 @@ for seq_len in seq_lens:
     for layer in layers:
         for hidden_size in hidden_sizes:
 
-            dir_name = "output/{}_{}_{}/".format(seq_len, layer, hidden_size)
+            dir_name = "output_2/{}_{}_{}/".format(seq_len, layer, hidden_size)
 
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
@@ -23,4 +23,4 @@ for seq_len in seq_lens:
             #log = open(dir_name+'log.txt', 'a')
             print("Starting with hyperparams seq_len: {} layer: {} hidden_size: {}".format(seq_len, layer, hidden_size))
 
-            main(directory='yamaha/tensors', outputDirectory=dir_name, n_epochs=80,seq_len=seq_len,layers=layer,hidden_size=hidden_size, use_gpu=True)
+            main(directory='yamaha/tensors', outputDirectory=dir_name, n_epochs=200,seq_len=seq_len,layers=layer,hidden_size=hidden_size, use_gpu=True)
